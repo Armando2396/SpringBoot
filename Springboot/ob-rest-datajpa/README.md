b92d3be61047030fb223f8a3ff25d66d2341053f
Armando2396 <armandomartinezbatista2@gmail.com>
2023-05-15 17:42:33 -0500

    agregando nuevo proyecto a la carpeta de springboot

---
+++ Springboot/ob-rest-datajpa/README.md
@@ -0,0 +1,18 @@
+## Spring Boot
+
+Proyecto Spring Boot con las dependencias /starters:
+
+* H2
+* Spring Data JPA
+* Spring web
+* Spring Boot Dev Tools
+
+
+Aplicacion API REST con acceso a base de datos  H2  para persistir la informacion
+el acceso  se puede  realizar  desde postman o navegador.
+ 
+## Entidad book
+
+1. Book
+2. BookRepository
+3. BookController
---
+++ Springboot/ob-rest-datajpa/src/main/java/com/example/obrestdatajpa/Book.java
@@ -0,0 +1,95 @@
+package com.example.obrestdatajpa;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+@Entity
+public class Book {
+    //atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private Integer pages;
+    private Double price;
+    private LocalDate realeaseDate;
+    private boolean online;
+
+    //constructores
+    public Book(){
+
+    }
+
+    public Book(Long id, String title, String author, Integer pages, Double price, LocalDate realeaseDate, boolean online) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.price = price;
+        this.realeaseDate = realeaseDate;
+        this.online = online;
+    }
+
+    //getter y setter
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Integer getPages() {
+        return pages;
+    }
+
+    public void setPages(Integer pages) {
+        this.pages = pages;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public LocalDate getRealeaseDate() {
+        return realeaseDate;
+    }
+
+    public void setRealeaseDate(LocalDate realeaseDate) {
+        this.realeaseDate = realeaseDate;
+    }
+
+    public boolean isOnline() {
+        return online;
+    }
+
+    public void setOnline(boolean online) {
+        this.online = online;
+    }
+
+}
